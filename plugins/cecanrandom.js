b51dfd2b53780fe640973134e8df2f09aad4b68e
WANDAPRASETYA07 <110240616+WANDAPRASETYA07@users.noreply.github.com>
2022-08-16 23:14:52 +0700

    Update cecanrandom.js

--- plugins/cecanrandom.js
+++ plugins/cecanrandom.js
@@ -6,7 +6,7 @@ fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/cecan.txt')
 let handler = async (m, { conn }) => {
     let img = arr[Math.floor(Math.random() * arr.length)]
     if (!img) throw img
-    await conn.sendFile(m.chat, img, '','© Nih Jgn lupa subscribe Channel MBY?OFFICIAL https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g👍', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '','© Nih Ajg', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['cecanrandom']
 handler.tags = ['asupan']
